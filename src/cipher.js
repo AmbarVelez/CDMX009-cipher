f4558339812cf678130ef7c80d73e4d53d4c3878
Ambar Vélez <ambar.zavelez@gmail.com>
2020-02-17 00:03:57 -0500

    se agrego el titulo en pagina principal, se logro hacer funcionar los botones de copiar y borrar. Se agrego el cipher completo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,39 @@
-const cipher = {
-  // ...
-};
+/*funcion numero de desplazamientos*/
+function desplazamientos(){
+  let numero = document.getElementById("numero").value;
+}
 
-export default cipher;
+/*funcion para cifrar*/
+function cifrar(elemento){
+  let texto = document.getElementById("textoEscrito").value;
+  let texto_cifrado = "";
+  let desplazamiento = document.getElementById("numero").value;
+
+  for (let i = 0; texto.length > i; i++){
+    let codigoASC = texto[i].charCodeAt();
+    if (codigoASC >= 65 && codigoASC <= 90){
+      texto_cifrado = texto_cifrado + String.fromCharCode((codigoASC - 65 + Number(desplazamiento)) % 26 + 65)
+    } else if (codigoASC >= 97 && codigoASC <= 122){
+      texto_cifrado = texto_cifrado + String.fromCharCode((codigoASC - 97 + Number(desplazamiento)) % 26 + 97)
+    }
+  }
+  document.getElementById("resultados").innerHTML = texto_cifrado;
+}
+
+
+/*Funcion para descifrar */
+function descifrar(elemento){
+  let texto = document.getElementById("textoEscrito").value;
+  let texto_descifrado = "";
+  let desplazamiento = document.getElementById("numero").value;
+
+  for (let i = 0; texto.length > i; i++){
+    let codigoASC = texto[i].charCodeAt();
+    if (codigoASC >= 65 && codigoASC <= 90){
+      texto_descifrado = texto_descifrado + String.fromCharCode((codigoASC + 65 - Number(desplazamiento)) % 26 + 65)
+    } else if (codigoASC >= 97 && codigoASC <= 122){
+      texto_descifrado = texto_descifrado + String.fromCharCode((codigoASC + 85 - Number(desplazamiento)) % 26 + 97)
+    }
+  }
+  document.getElementById("resultados").innerHTML = texto_descifrado;
+ }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-function mostrarSiguienteBloque(bloqueId) {
-document.getElementById(bloqueId).style.display = "block";
+/*funcion para cambios de pagina*/
+function mostrarSiguienteContenedor(idDelContenedorParaMostrar, idDelContenedorParaOcultar) {
+document.getElementById(idDelContenedorParaMostrar).style.display = "block";
+
+document.getElementById(idDelContenedorParaOcultar).style.display ="none";
+}
+
+/*funcion para copiar*/
+function copiarResultado() {
+  let elementoResultado = document.getElementById("resultados");
+  let range = document.createRange();
+  range.selectNode(elementoResultado);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
 }
